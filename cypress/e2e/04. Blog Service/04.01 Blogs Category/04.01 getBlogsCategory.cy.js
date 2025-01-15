2e0173565fd8bf6b5943f00aa793452eff4be2ee
Anonno Das <anonnodas97@gmail.com>
2025-01-15 20:04:56 +0600

    get blogs category

---
+++ cypress/e2e/04. Blog Service/04.01 Blogs Category/04.01 getBlogsCategory.cy.js
@@ -0,0 +1,30 @@
+describe("Get blogs category with status code 200", () => {
+  it("Should successfully fetch blog category", () => {
+    cy.request({
+      method: "GET",
+      url: "/blog/category/get",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        // expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
