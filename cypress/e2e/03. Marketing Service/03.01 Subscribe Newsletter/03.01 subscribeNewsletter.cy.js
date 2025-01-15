739bd066ecdefcb7f89649715676ea2e5cea5524
Anonno Das <anonnodas97@gmail.com>
2025-01-15 19:36:26 +0600

    add subscribe newsletter

---
+++ cypress/e2e/03. Marketing Service/03.01 Subscribe Newsletter/03.01 subscribeNewsletter.cy.js
@@ -0,0 +1,30 @@
+describe.skip("Post subscribe newsletter with status code 200", () => {
+  it("Should successfully subscribe newsletter", () => {
+    cy.request({
+      method: "POST",
+      url: "/marketing/newsletter/subscribe",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
