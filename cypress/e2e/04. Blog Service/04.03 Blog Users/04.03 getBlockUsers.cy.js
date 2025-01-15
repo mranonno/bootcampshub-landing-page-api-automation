8d5023a93ada470a9c25f8a02a798a1ff3cd836a
Anonno Das <anonnodas97@gmail.com>
2025-01-15 20:35:11 +0600

    add blog users

---
+++ cypress/e2e/04. Blog Service/04.03 Blog Users/04.03 getBlockUsers.cy.js
@@ -0,0 +1,30 @@
+describe("Get blog users with status code 200", () => {
+  it("Should successfully fetch blog users", () => {
+    cy.request({
+      method: "GET",
+      url: "/blog/users",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
